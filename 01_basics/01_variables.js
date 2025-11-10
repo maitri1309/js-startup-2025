14d1ffd882bde92dbcdabaced6c5a4d0f76acb10
maitri1309 <dakoriamaitri@gmail.com>
2025-11-10 09:00:03 +0000

    datatypes and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=1001
+let accountEmail="abc@gmail.com"
+var accountPassword="12345"
+accoutnCity="Surat"
+let accountState;
+
+console.log(accountId);
+
+//accountId=1002
+accountEmail="qwe@gmail.com"
+accountPassword="23456"
+accoutnCity="ahmedabad"
+console.table([accountId,accountEmail,accountPassword,accoutnCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all js code as a newer version
+//alert(3+3)
+
+console.log(3
+    +
+    3
+);//code readability should be high
+console.log("maitri");
+
+
+let name="maitri";
+let age=21
+let isLoggedIn=false;
+//number 2 pwr 53
+//bigint
+// string=""
+//boolean=true/false
+// null =standalone value
+//undefined=symbol=unique
+
+console.log(typeof "maitri");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
