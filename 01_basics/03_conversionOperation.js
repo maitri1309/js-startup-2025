4f1f1cf233861ea116efd501f97a77cf3b0efd00
maitri1309 <dakoriamaitri@gmail.com>
2025-11-10 13:37:12 +0000

    conversionoperations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+//let score="33abc";
+//let score=null
+//let score=undefined
+//let score=true
+//let score=false
+let score="maitri"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+//33=33
+//33abc=NaN
+//true=1,false=0
+//""=flase
+//"maitri"=true
+
+let isLoggedIn= "maitri"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+let someNumber=33;
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
