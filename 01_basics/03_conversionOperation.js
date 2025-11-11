6d4981e48aed7b89a4cfdf51ed7100380e98f7dc
maitri1309 <dakoriamaitri@gmail.com>
2025-11-11 08:44:42 +0000

    conversionnss

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -3,11 +3,11 @@
 //let score=undefined
 //let score=true
 //let score=false
-let score="maitri"
+let score = "maitri"
 console.log(typeof score);
 console.log(typeof (score));
 
-let valueInNumber=Number(score);
+let valueInNumber = Number(score);
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
 //33=33
@@ -16,14 +16,54 @@ console.log(valueInNumber);
 //""=flase
 //"maitri"=true
 
-let isLoggedIn= "maitri"
-let booleanIsLoggedIn=Boolean(isLoggedIn)
+let isLoggedIn = "maitri"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn)
 
 
-let someNumber=33;
-let stringNumber=String(someNumber)
+let someNumber = 33;
+let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+//******* operations *******
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);*/
+
+let str1 = "hello "
+let str2 = "Maitri"
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + "2")
+console.log("1" + 2 + 2)
+console.log(2 + 2 + "1")
+
+console.log(true)
+console.log(+true);
+//console.log(true+);
+console.log(+"");
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+let gameCounter=100
+gameCounter++
+// gameCounter--
+// ++gameCounter
+// --gameCounter
+console.log(gameCounter);
+
 
 
