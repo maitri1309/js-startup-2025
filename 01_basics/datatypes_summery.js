815b15618801662919ebe0913f81d678d2c10aa1
maitri1309 <dakoriamaitri@gmail.com>
2025-11-11 12:22:57 +0000

    done

---
+++ 01_basics/datatypes_summery.js
@@ -0,0 +1,30 @@
+// primitive datatypes
+/* 7 types: String,number,boolean,null,undefined,symbol,bigint*/
+
+//Examples:
+const scroe=100
+const scoreValue=100.3
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id===anotherId)
+const bigNumber=25858455521515554n
+console.log(typeof bigNumber);
+
+// non primitive / reference type
+/* types:Array,object,functions */
+
+//Examples:
+const fruits=["apple","mango","banana"];
+let fruObj={
+    name:"apple",
+    coloe:"red",
+};
+const myFunction=function(){
+    console.log("hello world");
+}
+console.log(typeof outsideTemp)
+console.log(typeof scoreValue)
+console.log(typeof myFunction);
