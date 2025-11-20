3644212cec8ef8813fc4cb68f47223fbe69faf2a
maitri1309 <dakoriamaitri@gmail.com>
2025-11-20 12:37:09 +0000

    arrow done

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,43 @@
+const user = {
+    username: "maitri",
+    price: 999,
+
+    welcomeMessage: function () {
+        console.log(`${this.username},Welcome to website`);
+        console.log(this);
+    }
+}
+user.welcomeMessage()
+user.username = "sam"
+user.welcomeMessage()
+console.log(this);
+
+
+// function chai(){
+//     let username="maitriiii"
+//     console.log(this.username);
+// }
+// chai();
+
+// const chai=function(){
+//     let username="maitriiii"
+//     console.log(this.username);
+// }
+const chai = () => {
+    let username = "maitriiii"
+    //console.log(this.username);
+    console.log(this);
+}
+
+//chai()
+
+// const addTwo=(num1,num2)=>{
+//     return num1+num2;
+// }
+//const addTwo = (num1, num2) => num1 + num2;
+//const addTwo = (num1, num2) => (num1 + num2);
+const addTwo = (num1, num2) => ({username:"maitry"})
+console.log(addTwo(10, 2))
+
+const myArray=[1,2,3,4,5]
+//myArray.forEach()
