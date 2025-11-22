49d3dccb3ccb5944e990306fd1afa2977775398e
maitri1309 <dakoriamaitri@gmail.com>
2025-11-22 11:32:54 +0000

    one and two done

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+//while
+let index = 0
+while (index <= 10) {
+    console.log(`value of index is ${index}`);
+    index = index + 2
+}
+
+let myArray=['rakesh','priyanka','maitri']
+let arr=0
+while (arr<myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr=arr+1
+    
+}
+
+//do while
+//let score=11
+let score=1
+do {
+    console.log(`score is ${score}`);
+    score=score+1
+} while (score<=10);
