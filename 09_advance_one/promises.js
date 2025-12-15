e48344e9b2e2d15fb0db96a78a3e12dd1233435f
maitri1309 <dakoriamaitri@gmail.com>
2025-12-15 06:25:13 +0000

    promises doneeee

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -57,4 +57,32 @@ const promiseFive = new Promise(function (resolve, reject) {
             reject('ERROR: Maitri went wrong')
         }
     }, 1000)
-})
+});
+
+async function consumePromiseFive() {
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+consumePromiseFive()
+
+// async function getAllUser() {
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ", error);
+//     }
+// }
+// getAllUser()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => {
+        return response.json()
+    }).then((data) => {
+        console.log(data);
+    }).catch((error) => console.log(error))
